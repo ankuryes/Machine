d6691630316c5090e59039eafff2c4d7e202e1ee
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-06-27 21:57:14 +0530

    Update script.py

--- script.py
+++ script.py
@@ -40,7 +40,7 @@ def Main_dta():
 	Cook = response.cookies.get_dict()
 	soup = BeautifulSoup(response.content, 'lxml')
 	ad_form,Token,Token_unlocked = Encode1.quote(soup.find('input',{'name': 'ad_form_data'})['value']),Encode1.quote(soup.find('input',{'name': '_Token[fields]'})['value']),Encode1.quote(soup.find('input',{'name': '_Token[unlocked]'})['value'])
-	sleep(5)
+	sleep(3)
 	headers = {"accept": "application/json, text/javascript, */*; q=0.01", "x-requested-with": "XMLHttpRequest", "user-agent": "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36", "content-type": "application/x-www-form-urlencoded; charset=UTF-8", "origin": "https://za.gl", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty", "referer": "https://za.gl/yesankur", "accept-language": "en-GB,en;q=0.9"}
 	cookies = {"AppSession": Cook['AppSession'], "zagl_publisher": Cook['zagl_publisher'], "scr": Cook['scr'], "csrfToken": Cook['csrfToken'], "visitor": Cook['visitor'], "hash": Cook['hash'], "sls": "0", "ref": "admin", "browserprint": "724483257531cc15c2c696e1c3b033a425feed4e3c8d0bfd759c5fe964cc61f5", "overlay": "1", "ab": "2", "sb_main_29b552ac181cd0b221e0fcc9e06f6754": "1", "slv": "-1", "sb_count_29b552ac181cd0b221e0fcc9e06f6754": "1", "pbpr0tpuw4isk85t8yg3jb2lj5vqf": "wilfulpessimistic.com", "_ga": "GA1.2.1726704085.1610002127", "_gid": "GA1.2.394003706.1610002127", "_gat_gtag_UA_120643151_1": "1"}
 	data = f"_method=POST&_csrfToken={Cook['csrfToken']}&ad_form_data={ad_form}&_Token%5Bfields%5D={Token}&_Token%5Bunlocked%5D={Token_unlocked}"
