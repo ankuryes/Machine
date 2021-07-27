3ec8b7ce0df8d3c41620560245f09044cbd6e12c
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-07-27 10:56:37 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -6,6 +6,8 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 urls = []
 for i in range(9): urls.append(str(f"https://zagl{i+1}.ankurkumar8.repl.co"))
 while True:
-  for i in urls: requests.get(i,verify=False)
-  sleep(10800)
-  print(Done)
+  try:
+    for i in urls: requests.get(i,verify=False)
+    sleep(10800)
+  except: print("loop_error")
+  print("Done")
