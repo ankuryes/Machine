2feb88e0425bbbcfb599eefdcfddeef81ea42199
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-09-08 10:25:27 +0530

    Update script.py

--- script.py
+++ script.py
@@ -8,52 +8,47 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 while True:
     try:
         ip = str(requests.get("http://ipinfo.io/ip").text)
-        url = "https://za.gl/links/go"
-        #___________Url-Selector__________________
+        
         Mn_urls = str(requests.get("https://raw.githubusercontent.com/AnkurKumarji/Machine/feat/add-feedback-section/Zagl.Links").text).splitlines()
         Selected_Link = str(requests.get("https://Armitage.ankurkumar8.repl.co/").text).split(".")
-
         if int(Selected_Link[1]) >= len(Mn_urls):
             requests.get("https://Armitage.ankurkumar8.repl.co/reset.php");Selected_Url = Mn_urls[0]
         else:
             Selected_Url = Mn_urls[int(Selected_Link[1])];print(len(Mn_urls), Selected_Link, Selected_Url)
-        #__________Url-Selector__________Finished
+        Selected_Url = Selected_Url.replace("zee.gl","za.gl")
+
+        req = requests.Session()
+        useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+
+        headers = {"user-agent": useragent,"accept": "text/html", "sec-fetch-site": "none", "sec-fetch-mode": "navigate", "sec-fetch-dest": "document","accept-language": "en-GB,en;q=0.9"}
+        res1 = req.get(Selected_Url, headers=headers)
+        soup = BeautifulSoup(res1.text, 'html.parser')
+        Token = Encode1.quote(soup.find('input', {'name': '_Token[fields]'})['value'])
+        Token_unlocked = Encode1.quote(soup.find('input', {'name': '_Token[unlocked]'})['value'])
+        givenX = Encode1.quote(soup.find('input', {'name': 'givenX'})['value'])
+        givenY = Encode1.quote(soup.find('input', {'name': 'givenY'})['value'])
+        data = str(soup.find('button',{'id': 'greendot'}).find('img')['src']).split('png;base64,')[1]
+        base64data = list(str(requests.post("https://ImageCord.ankurkumar8.repl.co",data=data).text).replace('[','').replace(']','').replace(' ','').split(','))
 
 
-        #__________Req1___________________________
-        headers = {"user-agent": "Mozilla/5.0 (Linux; Android 10; Redmi Note 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36","accept": "text/html", "sec-fetch-site": "none", "sec-fetch-mode": "navigate", "sec-fetch-dest": "document","accept-language": "en-GB,en;q=0.9"}
-        req1 = requests.get(Selected_Url, headers=headers)
-        Cook = req1.cookies.get_dict()
-        #__________Req1__________________Finished
-
-
-        #__________Var-Extract___________________
-        #_______Using_BS4__________
-        # from bs4 import BeautifulSoup
-        # soup = BeautifulSoup(req1.content, 'lxml')
-        # ad_form = Encode1.quote(soup.find('input', {'name': 'ad_form_data'})['value'])
-        # Token = Encode1.quote(soup.find('input', {'name': '_Token[fields]'})['value'])
-        # Token_unlocked = Encode1.quote(soup.find('input', {'name': '_Token[unlocked]'})['value'])
-
-        #_____Using_String Method___
-        ad_form = Encode1.quote(str(req1.text[str(req1.text).find('ad_form_data" value="')+21:str(req1.text).find('ad_form_data" value=')+450]).split('" />')[0])
-        Token = Encode1.quote(str(req1.text[str(req1.text).find('_Token[fields]" autocomplete="off" value="')+42:str(req1.text).find('_Token[fields]" autocomplete="off" value="')+200]).split('" />')[0])
-        Token_unlocked = Encode1.quote(str(req1.text[str(req1.text).find('_Token[unlocked]" autocomplete="off" value="')+44:str(req1.text).find('_Token[unlocked]" autocomplete="off" value="')+200]).split('" />')[0])
-        #__________Var-Extract___________Finished
-
+        headers = {'Host': 'za.gl','User-Agent': useragent,'Accept': 'text/html','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded','Origin': 'https://za.gl','Referer': Selected_Url}
+        data = f'_method=POST&_csrfToken={res1.cookies.get_dict()["csrfToken"]}&ref=&f_n=slc&dot=1&givenX={givenX}&givenY={givenY}&X={base64data[0]}&Y={base64data[1]}&_Token%5Bfields%5D={Token}&_Token%5Bunlocked%5D={Token_unlocked}'
+        res2 = req.post(Selected_Url, headers=headers,data=data,timeout=10)
+        soup = BeautifulSoup(res2.content, 'lxml')
+        ad_form = Encode1.quote(soup.find('input', {'name': 'ad_form_data'})['value'])
+        Token = Encode1.quote(soup.find('input', {'name': '_Token[fields]'})['value'])
+        Token_unlocked = Encode1.quote(soup.find('input', {'name': '_Token[unlocked]'})['value'])
+        Cook = res1.cookies.get_dict()
+        Cook.update(res2.cookies.get_dict())
+        Cook.update({'ab':'2', 'ref':'adimin', 'sls':'0'})
 
         sleep(3)
 
-
-        #__________Main-Request___________________
-        headers = {"accept": "application/json, text/javascript, */*; q=0.01", "x-requested-with": "XMLHttpRequest","user-agent": "Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36","content-type": "application/x-www-form-urlencoded; charset=UTF-8", "origin": "https://za.gl","sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty","referer": Selected_Url, "accept-language": "en-GB,en;q=0.9"}
-        cookies = {"AppSession": Cook['AppSession'],"zagl_publisher": Cook['zagl_publisher'],"scr": Cook['scr'],"csrfToken": Cook['csrfToken'],"visitor": Cook['visitor'],"hash": Cook['hash'],"sls": "0","ref": "admin","browserprint": "8b009e12f3bad328ce10f5803a223ca11621cb5cdb5a207dca519f2e50565610","overlay": "1","ab": "2","sb_main_29b552ac181cd0b221e0fcc9e06f6754": "1","slv": "-1","sb_count_29b552ac181cd0b221e0fcc9e06f6754": "1","pbpr0tpuw4isk85t8yg3jb2lj5vqf": "wilfulpessimistic.com","_ga": "GA1.2.1726704085.1610002127","_gid": "GA1.2.394003706.1610002127","_gat_gtag_UA_120643151_1": "1"}
+        headers = {"accept": "application/json, text/javascript, */*; q=0.01", "x-requested-with": "XMLHttpRequest","user-agent": useragent,"content-type": "application/x-www-form-urlencoded; charset=UTF-8", "origin": "https://za.gl","sec-fetch-site": "same-origin", "sec-fetch-mode": "cors", "sec-fetch-dest": "empty","referer": Selected_Url}
+        cookies = {"AppSession": Cook['AppSession'],"zagl_publisher": Cook['zagl_publisher'],"scr": Cook['scr'],"csrfToken": Cook['csrfToken'],"visitor": Cook['visitor'],"hash": Cook['hash'],"sls": "0","ref": "admin","browserprint": "8b009e12f3bad328ce10f5803a223ca11621cb5cdb5a207dca519f2e50565610","overlay": "1","ab": "2","sb_main_29b552ac181cd0b221e0fcc9e06f6754": "1","slv": "-1","sb_count_29b552ac181cd0b221e0fcc9e06f6754": "1"}
         data = f"_method=POST&_csrfToken={Cook['csrfToken']}&ad_form_data={ad_form}&_Token%5Bfields%5D={Token}&_Token%5Bunlocked%5D={Token_unlocked}"
-        mn_req = requests.post(url, headers=headers, cookies=cookies, data=data)
-        #__________Main-Request__________Finished
-
-        print("Zagl",mn_req.text)
-
+        mn_req = requests.post("https://za.gl/links/go", headers=headers, cookies=Cook, data=data, timeout=3)
+        print(mn_req.text)
 
         # ----------------------------------------------------------------------------------
 
