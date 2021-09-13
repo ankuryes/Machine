5252e832d50b0bbfbfbe44226d6334945087325d
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-09-13 14:58:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -8,32 +8,35 @@ from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 def internet_on():
-    try:
-        requests.get('http://ipinfo.io/ip')
-        return True
+    try: requests.get('https://www.google.com/');return True
     except: return False
 
 while True:
     try:
         useragent = "Mozilla/5.0 (Linux; Android 11; RMX3241) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36"
 
-        ip = str(requests.get("http://ipinfo.io/ip").text)
+        try: ip = str(requests.get("https://jsonip.com/").json()['ip'])
+        except: ip = str(requests.get("http://ipinfo.io/ip").text)
+
         print("+",ip)
 
-        #urlshortx.com Req1
-        urlshortx = "https://m.bdnewsx.com/git"
-        shortx_headers = {'Host': 'm.bdnewsx.com','User-Agent': useragent,'Accept': 'text/html','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Referer': 'https://techrfour.com/2021/05/17/top-reason-to-buy-a-child-education-insurance-plan/'}
-        shortx_res1 = requests.get(urlshortx , headers=shortx_headers)
-        shortx_Cook = shortx_res1.cookies.get_dict()
-        shortx_Cook.update({'ab': '2','prefetchAd_3113442': 'true'})
+        try:
+            #urlshortx.com Req1
+            urlshortx = str(requests.get("https://urlshortxSelectLink.ankurkumar8.repl.co").text)
+            shortx_headers = {'Host': 'm.bdnewsx.com','User-Agent': useragent,'Accept': 'text/html','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Referer': 'https://techrfour.com/2021/05/17/top-reason-to-buy-a-child-education-insurance-plan/'}
+            shortx_res1 = requests.get(urlshortx , headers=shortx_headers)
+            shortx_Cook = shortx_res1.cookies.get_dict()
+            shortx_Cook.update({'ab': '2','prefetchAd_3113442': 'true'})
 
-        soup = BeautifulSoup(shortx_res1.text, 'html.parser')
-        shortx_ad_form = Encode1.quote(soup.find('input', {'name': 'ad_form_data'})['value'])
-        shortx_Token = Encode1.quote(soup.find('input', {'name': '_Token[fields]'})['value'])
-        shortx_Token_unlocked = Encode1.quote(soup.find('input', {'name': '_Token[unlocked]'})['value'])
+            soup = BeautifulSoup(shortx_res1.text, 'html.parser')
+            shortx_ad_form = Encode1.quote(soup.find('input', {'name': 'ad_form_data'})['value'])
+            shortx_Token = Encode1.quote(soup.find('input', {'name': '_Token[fields]'})['value'])
+            shortx_Token_unlocked = Encode1.quote(soup.find('input', {'name': '_Token[unlocked]'})['value'])
+        except:pass
 
         #Droplink.co Req1
-        drop_req1 = requests.get("https://droplink.co/Zagl")
+        drop_Select_link = str(requests.get("https://selectlink.ankurkumar8.repl.co/").text)
+        drop_req1 = requests.get(drop_Select_link)
         drop_main_cook = drop_req1.cookies.get_dict()
 
         drop_headers = {'Host': 'yoshare.net', 'User-Agent': useragent, 'Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded'}
@@ -43,11 +46,11 @@ while True:
         redi_url = str(soup.find("form", {"id": "yuidea"})['action'])
 
         drop_headers = {'Host': 'yoshare.net', 'User-Agent': useragent, 'Accept': 'text/html', 'Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate', 'Content-Type': 'application/x-www-form-urlencoded','Origin': 'https://yoshare.net', 'Referer': 'https://yoshare.net/', 'Sec-Fetch-User': '?1'}
-        drop_data = 'clikarlink=Zagl&yuidea=&verify=Click+here+to+continue'
+        drop_data = f'clikarlink={drop_Select_link.split("droplink.co/")[1]}&yuidea=&verify=Click+here+to+continue'
         drop_req3 = requests.post(f'{redi_url}', headers=drop_headers, data=drop_data)
 
         drop_headers = {'Host': 'droplink.co', 'User-Agent': useragent, 'Accept': 'text/html', 'Accept-Encoding': 'gzip, deflate','Referer': 'https://yoshare.net/', 'Sec-Fetch-User': '?1'}
-        drop_req4 = requests.get('https://droplink.co/Zagl', headers=drop_headers, cookies=drop_main_cook)
+        drop_req4 = requests.get(drop_Select_link, headers=drop_headers, cookies=drop_main_cook)
         soup = BeautifulSoup(drop_req4.text, 'html.parser')
         drop_ad_form = Encode1.quote(soup.find('input', {'name': 'ad_form_data'})['value'])
         drop_Token = Encode1.quote(soup.find('input', {'name': '_Token[fields]'})['value'])
@@ -55,12 +58,7 @@ while True:
 
 
         #Za.gl Req1
-        Mn_urls = str(requests.get("https://raw.githubusercontent.com/AnkurKumarji/Machine/feat/add-feedback-section/Zagl.Links").text).splitlines()
-        Selected_Link = str(requests.get("https://Armitage.ankurkumar8.repl.co/").text).split(".")
-        if int(Selected_Link[1]) >= len(Mn_urls):
-            requests.get("https://Armitage.ankurkumar8.repl.co/reset.php");Selected_Url = Mn_urls[0].replace("zee.gl", "za.gl")
-        else:
-            Selected_Url = Mn_urls[int(Selected_Link[1])].replace("zee.gl", "za.gl")
+        Selected_Url = str(requests.get("https://SelectZagl.ankurkumar8.repl.co").text)
 
         req = requests.Session()
         zagl_headers = {"user-agent": useragent, "accept": "text/html", "sec-fetch-site": "none", "sec-fetch-mode": "navigate","sec-fetch-dest": "document", "accept-language": "en-GB,en;q=0.9"}
@@ -84,24 +82,26 @@ while True:
         zagl_Cook.update(zagl_res2.cookies.get_dict())
         zagl_Cook.update({'ab': '2', 'ref': 'adimin', 'sls': '0', 'overlay': '1'})
 
-        sleep(3)
+        sleep(4)
 
         #Earning requests...........................................................................
         #Droplink
         drop_main_cook['ab'] = '2'
-        drop_headers = {'Host': 'droplink.co', 'User-Agent': useragent, 'Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'X-Requested-With': 'XMLHttpRequest','Origin': 'https://droplink.co', 'Referer': 'https://droplink.co/Zagl', 'Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'same-origin'}
+        drop_headers = {'Host': 'droplink.co', 'User-Agent': useragent, 'Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'X-Requested-With': 'XMLHttpRequest','Origin': 'https://droplink.co', 'Referer': drop_Select_link, 'Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'same-origin'}
         drop_data = f"_method=POST&_csrfToken={drop_main_cook['csrfToken']}&ad_form_data={drop_ad_form}&_Token%5Bfields%5D={drop_Token}&_Token%5Bunlocked%5D={drop_Token_unlocked}"
 
         drop_Mn_req = requests.post('https://droplink.co/links/go', headers=drop_headers, cookies=drop_main_cook, data=drop_data)
         if "Go With earning :)" in drop_Mn_req.text:print('Req1')
         else: print('Req1_Err.')
 
-        #urlshortx
-        shortx_headers = {'Host': 'm.bdnewsx.com','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0','Accept': 'application/json, text/javascript, */*; q=0.01','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8','X-Requested-With': 'XMLHttpRequest','Origin': 'https://m.bdnewsx.com','Referer': urlshortx,'Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'same-origin','Te': 'trailers'}
-        shortx_data = f'_method=POST&_csrfToken={shortx_Cook["csrfToken"]}&ad_form_data={shortx_ad_form}&_Token%5Bfields%5D={shortx_Token}&_Token%5Bunlocked%5D={shortx_Token_unlocked}'
-        shortx_Mn_req = requests.post('https://m.bdnewsx.com/links/go', headers=shortx_headers, cookies=shortx_Cook, data=shortx_data)
-        # if "Go With earning :)" in shortx_Mn_req.text:print('Req working')
-        # else: print('Req_shortx_Err. short')
+        try:
+            #urlshortx
+            shortx_headers = {'Host': 'm.bdnewsx.com','User-Agent': useragent,'Accept': 'application/json, text/javascript, */*; q=0.01','Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8','X-Requested-With': 'XMLHttpRequest','Origin': 'https://m.bdnewsx.com','Referer': urlshortx,'Sec-Fetch-Dest': 'empty','Sec-Fetch-Mode': 'cors','Sec-Fetch-Site': 'same-origin','Te': 'trailers'}
+            shortx_data = f'_method=POST&_csrfToken={shortx_Cook["csrfToken"]}&ad_form_data={shortx_ad_form}&_Token%5Bfields%5D={shortx_Token}&_Token%5Bunlocked%5D={shortx_Token_unlocked}'
+            shortx_Mn_req = requests.post('https://m.bdnewsx.com/links/go', headers=shortx_headers, cookies=shortx_Cook, data=shortx_data)
+            # if "Go With earning :)" in shortx_Mn_req.text:print('Req working')
+            # else: print('Req_shortx_Err. short')
+        except:pass
 
         #zagl
         zagl_headers = {"accept": "application/json, text/javascript, */*; q=0.01", "x-requested-with": "XMLHttpRequest","user-agent": useragent, "content-type": "application/x-www-form-urlencoded; charset=UTF-8","origin": "https://za.gl", "sec-fetch-site": "same-origin", "sec-fetch-mode": "cors","sec-fetch-dest": "empty", "referer": Selected_Url}
@@ -113,8 +113,9 @@ while True:
         print('Complete')
 
         while True:
-            new_ip = str(requests.get("http://ipinfo.io/ip").text)
-            if new_ip != ip:break
+            try : new_ip = str(requests.get("https://jsonip.com/").json()['ip'])
+            except: new_ip = str(requests.get("http://ipinfo.io/ip").text)
+            if (new_ip!=ip) and (internet_on()==True):break
             sleep(3)
     except:
         while True:
