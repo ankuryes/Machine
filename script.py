b3db1d74b7d1ed79355aa9ee5822d5b5d3833e71
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-10-22 00:52:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,17 @@
 import requests
+from flask import Flask
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-try: ip = str(requests.get("https://jsonip.com/").json()['ip'])
-except: ip = str(requests.get("http://ipinfo.io/ip").text)
+app = Flask('')
 
-data = str(ip)
-dta = requests.post('https://Zagl1.ankurkumar8.repl.co/found',data=data).text
-print(str(dta))
+@app.route('/')
+def home():
+    try: ip = str(requests.get("https://jsonip.com/").json()['ip'])
+    except: ip = str(requests.get("http://ipinfo.io/ip").text)
+
+    data = str(ip)
+    dta = requests.post('https://Zagl1.ankurkumar8.repl.co/found', data=data).text
+    return str(dta)
+
+app.run(host='0.0.0.0', port=8080)
