2e87dbf2680635be299050f5eb07313b68ba62ad
AnkurKumarji <86411132+AnkurKumarji@users.noreply.github.com>
2021-12-30 22:54:42 +0530

    Update script.py

--- script.py
+++ script.py
@@ -20,7 +20,7 @@
 # app.run(host='0.0.0.0', port=8080)
 from os import system
 system("pip install bs4")
-ststem("pip install PIL")
+system("pip install PIL")
 import requests
 from time import sleep
 from bs4 import BeautifulSoup
